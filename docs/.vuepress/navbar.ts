5e3cd4901c88ad233558116dd862b3ec38356819
yunze <834363368@qq.com>
2025-08-21 11:32:37 +0800

    perf: 目录结构调整

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,6 +3,7 @@ import { defineNavbarConfig } from 'vuepress-theme-plume'
 export const navbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
+  { text: '面试突击', link: '/interview/README.md' },
   { text: '标签', link: '/blog/tags/' },
   { text: '归档', link: '/blog/archives/' },
   {
--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -2,11 +2,21 @@ import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
 
 const postgraduateNote = defineNoteConfig({
+  // 声明笔记的目录，相对于 `postgraduate.dir`，这里表示 `notes/postgraduate` 目录
   dir: 'postgraduate',
+  // 声明笔记的链接前缀，与 `notes.link` 拼接，这里表示 `/postgraduate/`
+  // 笔记内的所有文章会以 `/postgraduate/` 作为访问链接前缀。
   link: '/postgraduate',
+  // 配置 笔记侧边导航栏，用于导航向笔记内的所有文档
+  // 声明为 `auto` 的，将根据目录结构自动生成侧边栏导航
   sidebar: 'auto'
 })
 
+const interviewNote = defineNoteConfig({
+  dir: 'interview',
+  link: '/interview',
+  sidebar: 'auto'
+})
 
 const makeMoneyNote = defineNoteConfig({
   dir: 'random',
@@ -17,5 +27,5 @@ const makeMoneyNote = defineNoteConfig({
 export const notes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [postgraduateNote, makeMoneyNote],
+  notes: [postgraduateNote, interviewNote, makeMoneyNote],
 })
