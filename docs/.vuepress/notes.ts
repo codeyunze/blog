0f4c1962fe76c3bb836abbd0aab8ac63891ce772
yunze <834363368@qq.com>
2025-12-09 08:53:22 +0800

    perf: 目录调整

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -18,14 +18,8 @@ const interviewNote = defineNoteConfig({
   sidebar: 'auto'
 })
 
-const makeMoneyNote = defineNoteConfig({
-  dir: 'random',
-  link: '/random',
-  sidebar: ['', '黄金交易'],
-})
-
 export const notes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [postgraduateNote, interviewNote, makeMoneyNote],
+  notes: [postgraduateNote, interviewNote],
 })
--- docs/notes/random/README.md
+++
@@ -1,7 +0,0 @@
----
-title: 赚钱
-createTime: 2025/02/16 15:02:54
-permalink: /random/
----
-
-- [黄金交易](./黄金交易.md)
